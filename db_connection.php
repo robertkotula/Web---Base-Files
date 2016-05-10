ea3f9c7b4ee4ad626b60675ebc85fefb51307478
Robert Kotula <robertkotula@outlook.com>
2016-05-10 23:22:08 +0100

    Added new Files

---
+++ db_connection.php
@@ -0,0 +1,10 @@
+<?php
+//connect to the database using mysqli API
+$mysqli = new mysqli($host, $username, $pword, $database);
+if ($mysqli->connect_errno) {
+	echo "Failed to connect to MySQL: (" .
+	$mysqli->connect_errno . ") "
+	. $mysqli->connect_error;
+	die;
+}
+?>
